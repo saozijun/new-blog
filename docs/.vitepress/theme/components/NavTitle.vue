540a1f2f64ed31ce1d2e933228e6277548841495
saozijun <1922708711@qq.com>
2025-06-20 23:43:33 +0800

    修改

--- docs/.vitepress/theme/components/NavTitle.vue
+++ docs/.vitepress/theme/components/NavTitle.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="title">
-        <img width="50px" v-bind:src="img" alt="" />
-        {{ img }}
+        <img width="50px" v-bind:src="imgSrc" alt="" />
         <img width="30px" style="position: relative; top: -2px" src="../static/xiaoxin.gif" alt="" />
     </div>
 </template>
@@ -10,14 +9,21 @@
 import sign from "../static/sign.png";
 import wSign from "../static/w-sign.png";
 import { useData } from "vitepress";
-import { computed } from "vue";
+import { computed, ref, onMounted, watch } from "vue";
+
 const { isDark } = useData();
-const img = computed(()=>{
-    console.log(isDark.value ? wSign : sign);
-    
-    return isDark.value ? wSign : sign;
-})
-console.log("isDark", isDark.value);
+const img = computed(() => isDark.value ? wSign : sign);
+const imgSrc = ref(sign); // 默认使用light模式图片
+
+// 在客户端挂载后更新图片源
+onMounted(() => {
+    imgSrc.value = isDark.value ? wSign : sign;
+});
+
+// 监听暗黑模式变化
+watch(isDark, (newVal) => {
+    imgSrc.value = newVal ? wSign : sign;
+}, { immediate: true });
 </script>
 
 <style lang="scss" scoped>
