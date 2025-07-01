c1089381cddd8a4025327c1767d96080ee2026b5
saozijun <1922708711@qq.com>
2025-07-01 22:24:52 +0800

    修改

--- docs/.vitepress/theme/components/Test.vue
+++ docs/.vitepress/theme/components/Test.vue
@@ -5,9 +5,14 @@
         <div class="item" v-for="(item, index) in list" :key="index">
           <video v-if="item.type == 'video'" :src="item.src" muted loop autoplay playsinline></video>
           <img v-else :src="item.src" alt="" />
+          <div class="mask" ref="maskRef"></div>
+          <div class="text-info" ref="textInfoRef">
+            <p>标题 {{ index + 1 }}</p>
+            <span>我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述</span>
+          </div>
         </div>
       </div>
-      <div class="text" ref="textRef">图片展示</div>
+      <div class="text" ref="textRef">GSAP-模块测试</div>
       <div class="text2" ref="text2Ref">
         {{ currentText }}
       </div>
@@ -23,6 +28,8 @@ const containerRef = ref(null);
 const listRef = ref(null);
 const textRef = ref(null);
 const text2Ref = ref(null);
+const textInfoRef = ref(null);
+const maskRef = ref(null);
 const currentText = ref("图1");
 let COSURL = import.meta.env.VITE_APP_COS_URL
 let list = [{
@@ -89,7 +96,9 @@ const init = () => {
   });
   tl2.to(item[0], { scale: 1, duration: 0.2 }, 0);
   tl2.to(textRef.value, { scale: 0.5, duration: 0.1 }, 0);
-  tl2.to(textRef.value, { scale: 0.15, y: -350, duration: 0.1 }, 0.1);
+  tl2.to(textRef.value, { scale: 0.25, y: -350, duration: 0.1 }, 0.1);
+  tl2.to(maskRef.value, { opacity: 1, duration: 0.1}, 0.1);
+  tl2.to(textInfoRef.value, { opacity: 1, x: 0, duration: 0.1}, 0.1);
   tl2.to(text2Ref.value, { opacity: 1, y: 50, duration: 0.05 }, 0.15);
   // 计算平移值 列表项宽度 * 列表项数量 - 盒子之间间隔 * 列表项数量（需要减去一个，不然全部滑过头嘞）
   tl2.to(
@@ -121,8 +130,12 @@ const init = () => {
       display: flex;
       justify-content: center;
       align-items: center;
-      font-size: 16rem;
+      font-size: 8rem;
       mix-blend-mode: difference;
+      pointer-events: none;
+    }
+    .text{
+      min-width: fit-content !important;
     }
     .text2 {
       font-size: 2rem;
@@ -132,6 +145,37 @@ const init = () => {
       left: 50%;
       opacity: 0;
       transform: translate(-50%, 100px);
+    }
+    .mask{
+      position: absolute;
+      top: -320px;
+      left: -449px;
+      width: 150%;
+      height: 100%;
+      opacity: 0;
+      background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0) 90%);
+      transform: rotate(134deg);
+      z-index: 1;
+    }
+    .text-info{
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      font-size: 2rem;
+      z-index: 2;
+      padding: 26px 40px;
+      color: #fff;
+      opacity: 0;
+      line-height: 1.8;
+      transform: translateX(-50px);
+      span{
+        width: 280px;
+        display: block;
+        color: #999;
+        font-size: 1.2rem;
+      }
     }
     .list {
       width: 100%;
@@ -146,8 +190,12 @@ const init = () => {
         border-radius: 10px;
         flex-shrink: 0;
         background: #0000002a;
+        position: relative;
+        overflow: hidden;
+        cursor: pointer;
         &:nth-child(1) {
           transform: scale(1.7);
+          z-index: 111;
         }
         video {
           width: 100%;
